691b22ee823cb47f33b3b233bf06d1a7e51558c9
Ibrahim <Alaoopeyemi101@gmail.com>
2020-03-01 20:33:01 +0100

    Add Calculate function

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //listen for sumbmit
-document.getElementById("loan-form").addEventListener('submit', calculateResult);
+document.getElementById("loan-form").addEventListener('submit', function (e) {
+
+    //hide result
+    document.getElementById('results').style.display = 'none';
+
+    //show loading
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResult, 2000);
+
+    e.preventDefault();
+});
 
 
 
-function calculateResult(e) {
+function calculateResult() {
 
 
     const UIamount = document.getElementById('amount');
@@ -12,11 +23,63 @@ function calculateResult(e) {
     const UImonthlyPayment = document.getElementById('monthly-payment');
     const UItotalPayment = document.getElementById('total-payment');
     const UItotalInterest = document.getElementById('total-interest');
+  
+
+    const principal = parseFloat(UIamount.value);
+    const calcIntrest = parseFloat(UIinterest.value) / 100 / 12;
+    const calcPayments = parseFloat(UIyears.value) * 12;
+
+    //monthly payment calculation
  
+    const x = Math.pow(1 + calcIntrest, calcPayments);
+    const monthly = (principal*x*calcIntrest)/(x-1);
 
-    const principal = parseFloat(amount.value);
-    const calcIntrest = parseFloat(interest.value) / 100 / 12;
-    const calcPayments = parseFloat(years.value) * 12;
 
-    e.preventDefault();
+    if(isFinite(monthly)) {
+        UImonthlyPayment.value = monthly.toFixed(2);
+        UItotalPayment.value = (monthly * calcPayments).toFixed(2);
+        UItotalInterest.value = ((monthly * calcPayments) - principal).toFixed(2)
+
+        //show result
+        document.getElementById('results').style.display = 'block';
+        
+        //hide loader
+        document.getElementById("loading").style.display = 'none';
+
+
+    }else {
+        showError('Please Enter Valid Numbers')
+    }
 }  
+
+
+function showError(error) {
+
+    //hide result
+    document.getElementById('results').style.display = 'none';
+        
+    //hide loader
+    document.getElementById("loading").style.display = 'none';
+
+
+    // create div
+    const errorDiv = document.createElement('div');
+
+    //get element
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    //add class
+    errorDiv.className = 'alert alert-danger';
+
+    //textnode
+    errorDiv.appendChild(document.createTextNode(error))
+
+    //inserterror above heading
+    card.insertBefore(errorDiv, heading)
+
+    // clear error after 3sec
+    setTimeout(() => {
+        document.querySelector('.alert').remove();
+    }, 3000)
+}
